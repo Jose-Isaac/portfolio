c47f5510a5608c5d8549f4d4d145f7a0fc4eaa97
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-07-28 10:21:08 -0300

    Adicionado a estrutura da página da sessão

---
+++ components/WhatDoIDo/WhatDoIDo.js
@@ -0,0 +1,9 @@
+import utilStyles from '../../styles/utils.module.css';
+
+export default function WhatDoIDo() {
+  return (
+    <section>
+      <h1 className={utilStyles.titleSection}>O que faço?</h1>
+    </section>
+  );
+}
