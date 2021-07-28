5c40dd7b571cd5572d232101c94fee8963e275b6
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-07-28 12:24:57 -0300

    Adicionado estilização ao textos da sessão

--- components/WhatDoIDo/WhatDoIDo.js
+++ components/WhatDoIDo/WhatDoIDo.js
@@ -1,9 +1,28 @@
 import utilStyles from '../../styles/utils.module.css';
+import styles from './WhatDoIDo.module.css';
 
 export default function WhatDoIDo() {
   return (
-    <section>
+    <section className={utilStyles.marginSection}>
       <h1 className={utilStyles.titleSection}>O que faço?</h1>
+
+      <div className={styles.content}>
+        <div className={styles.cardContent}>
+          <h2>Front-end</h2>
+          <p>
+            Desenvolvo aplicações web utilizando HTML, CSS e Javascript com os
+            Criando sites responsivos, seguindo o principio do Mobile First.
+            frameworks React e Next.
+          </p>
+        </div>
+        <div className={styles.cardContent}>
+          <h2>Back-end</h2>
+          <p>
+            Aplicações utilizando NodeJS, Typescript e MySQL. Criação de API´s
+            para comunicação com front-end seguindo princípio de Clean Code.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
